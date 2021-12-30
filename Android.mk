4d05df7b3462dbe61325614ccc62515a6f489a7a
Aaron Kling <aaron@curvereality.com>
2021-12-29 21:50:41 -0600

    Android: Add makefile
    Change-Id: I14a6cde9f39f7e4939864d3e425abc4c58cb8091

---
+++ Android.mk
@@ -0,0 +1,33 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(TARGET_SURVIVE_MATH_BACKEND),)
+    SURVIVE_MATH_BACKEND := $(TARGET_SURVIVE_MATH_BACKEND)
+else
+    SURVIVE_MATH_BACKEND := eigen
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE                := libcnmatrix
+LOCAL_SRC_FILES             := src/cn_matrix.c
+LOCAL_MODULE_CLASS          := STATIC_LIBRARIES
+LOCAL_C_INCLUDES            := $(LOCAL_PATH)/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+LOCAL_PROPRIETARY_MODULE    := true
+
+LOCAL_CFLAGS := \
+    -Wno-error=unused-const-variable \
+    -Wno-error=unused-function \
+    -Wno-error=unused-parameter
+
+ifeq ($(SURVIVE_MATH_BACKEND),eigen)
+    LOCAL_SRC_FILES        += src/cn_matrix.eigen.cpp
+    LOCAL_CFLAGS           += -DUSE_EIGEN
+    LOCAL_HEADER_LIBRARIES += libeigen
+else ifeq ($(SURVIVE_MATH_BACKEND),blas)
+    # This needs a blas library to link, which is not easily available
+    LOCAL_SRC_FILES        += src/cn_matrix.blas.c
+else
+    $(error Unsupported cnmatrix backend: $(SURVIVE_MATH_BACKEND))
+endif
+
+include $(BUILD_STATIC_LIBRARY)
